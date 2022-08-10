18a20ce9a8746b978be752ebe08f34d9e1d5645f
Felipe Tavares <felipeat07@gmail.com>
2022-08-10 12:56:48 +0100

    refreshToken

--- services/api.ts
+++ services/api.ts
@@ -1,11 +1,45 @@
 import axios from 'axios'
-import { parseCookies } from 'nookies'
+import { parseCookies, setCookie } from 'nookies'
 
-const { authToken } = parseCookies()
+let { authToken } = parseCookies()
 
 export const api = axios.create({
     baseURL: 'http://localhost:3333',
     headers: {
         Authorization: `Bearer ${authToken}`
+    }
+})
+
+api.interceptors.response.use(response => {
+    return response
+}, error => {
+    if(error.response.status === 401){
+        if(error.response.data?.code === 'token.expired') {
+           let cookies = parseCookies()
+
+           const { authRefreshToken } = cookies
+
+           api.post('/refresh', {
+            authRefreshToken
+           }).then(response => {
+            
+            const { authToken } = response.data
+           
+            setCookie(undefined, 'authToken', authToken, {
+                maxAge: 60*60*24*30, //expira em 30 dias
+                path: '/', //todas as rotas tem acesso ao cookie
+            })
+            setCookie(undefined, 'authRefreshToken', response.data.authRefreshToken, {
+                maxAge: 60*60*24*30, //expira em 30 dias
+                path: '/', //todas as rotas tem acesso ao cookie
+            })
+
+
+           })
+
+
+        } else {
+            //deslogar usuário
+        }
     }
 })
