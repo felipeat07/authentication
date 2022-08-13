8b84c71fb73fe41691431599b0e4ceecf80c26a6
Felipe Tavares <felipeat07@gmail.com>
2022-08-13 13:16:31 +0100

    add SSRauth

--- services/api.ts
+++ services/api.ts
@@ -1,84 +1,95 @@
 import axios, { AxiosError } from 'axios'
+import { appendFile } from 'fs';
 import  Router  from 'next/router';
 import { destroyCookie, parseCookies, setCookie } from 'nookies'
 
 let isRefreshing = false;
 let failedRequestsQueue: any[] = [];
-let { authToken } = parseCookies()
-
-
-export const api = axios.create({
-    baseURL: 'http://localhost:3333',    
-})
-
-api.defaults.headers.common.Authorization = `Bearer ${authToken}`;
 
 
 
-api.interceptors.response.use(response => {
-    return response
-}, error => {
-    if(error.response.status === 401){
-        if(error.response.data?.code === 'token.expired') {
-           let cookies = parseCookies()
+export function setupAPIClient(ctx = undefined) {
+    let { authToken } = parseCookies(ctx)
 
-           const { authRefreshToken } = cookies
-           const originalConfig = error.config
-
-           if(!isRefreshing){
-            isRefreshing = true
-
-            api.post('/refresh', {
-                refreshToken: authRefreshToken
-               }).then(response => {
-                
-                const { token } = response.data
-               
-                setCookie(undefined, 'authToken', token, {
-                    maxAge: 60*60*24*30, //expira em 30 dias
-                    path: '/', //todas as rotas tem acesso ao cookie
-                })
-                setCookie(undefined, 'authRefreshToken', response.data.refreshToken, {
-                    maxAge: 60*60*24*30, //expira em 30 dias
-                    path: '/', //todas as rotas tem acesso ao cookie
-                })
+    const api = axios.create({
+        baseURL: 'http://localhost:3333',    
+    })
     
-                api.defaults.headers.common.Authorization = `Bearer ${token}`; 
-
-                failedRequestsQueue.forEach(request => request.onSuccess(token))
-                failedRequestsQueue = []
+    api.defaults.headers.common.Authorization = `Bearer ${authToken}`;
     
-               }).catch(err => {
-
-                failedRequestsQueue.forEach(request => request.onFailure(err))
-                failedRequestsQueue = []
-
-               }).finally(()=> {
-                isRefreshing = false
+    
+    
+    api.interceptors.response.use(response => {
+        return response
+    }, error => {
+        if(error.response.status === 401){
+            if(error.response.data?.code === 'token.expired') {
+               let cookies = parseCookies(ctx)
+    
+               const { authRefreshToken } = cookies
+               const originalConfig = error.config
+    
+               if(!isRefreshing){
+                isRefreshing = true
+    
+                api.post('/refresh', {
+                    refreshToken: authRefreshToken
+                   }).then(response => {
+                    
+                    const { token } = response.data
+                   
+                    setCookie(ctx, 'authToken', token, {
+                        maxAge: 60*60*24*30, //expira em 30 dias
+                        path: '/', //todas as rotas tem acesso ao cookie
+                    })
+                    setCookie(ctx, 'authRefreshToken', response.data.refreshToken, {
+                        maxAge: 60*60*24*30, //expira em 30 dias
+                        path: '/', //todas as rotas tem acesso ao cookie
+                    })
+        
+                    api.defaults.headers.common.Authorization = `Bearer ${token}`; 
+    
+                    failedRequestsQueue.forEach(request => request.onSuccess(token))
+                    failedRequestsQueue = []
+        
+                   }).catch(err => {
+    
+                    failedRequestsQueue.forEach(request => request.onFailure(err))
+                    failedRequestsQueue = []
+    
+                   }).finally(()=> {
+                    isRefreshing = false
+                   })
+               }
+    
+               return new Promise((resolve, reject) => {
+                    failedRequestsQueue.push({
+                        onSuccess: (token: string) => {
+                            originalConfig.headers['Authorization'] = `Bearer ${token}`;
+    
+                            resolve(api(originalConfig))
+                        },
+                        onFailure: (err: AxiosError) => {
+                            reject(err)
+                        }
+                    })
                })
-           }
-
-           return new Promise((resolve, reject) => {
-                failedRequestsQueue.push({
-                    onSuccess: (token: string) => {
-                        originalConfig.headers['Authorization'] = `Bearer ${token}`;
-
-                        resolve(api(originalConfig))
-                    },
-                    onFailure: (err: AxiosError) => {
-                        reject(err)
-                    }
-                })
-           })
-
-
-        } else {
-            destroyCookie(undefined, 'authToken')
-            destroyCookie(undefined, 'refreshAuthToken')
-
-            Router.push('/')
+    
+    
+            } else {
+                if(process.browser){
+                    destroyCookie(undefined, 'authToken')
+                    destroyCookie(undefined, 'refreshAuthToken')
+        
+                    Router.push('/')
+                }
+    
+            }
         }
-    }
+    
+        return Promise.reject(error);
+    })
 
-    return Promise.reject(error);
-})
+    return api;
+    
+}
