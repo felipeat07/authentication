df7fcfdee22c5651fdbcccd7e0656b6df4aad871
Felipe Tavares <felipeat07@gmail.com>
2022-08-14 13:16:51 +0100

    create a permission hook and component

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,6 +1,8 @@
 import { destroyCookie } from "nookies"
 import { useContext, useEffect } from "react"
+import { Can } from "../components/Can"
 import { AuthContext } from "../contexts/AuthContext"
+import { useCan } from "../hooks/useCan"
 import { setupAPIClient } from "../services/api"
 import { api } from "../services/apiClient"
 import { withSSRAuth } from "../utils/withSSRAuth"
@@ -11,20 +13,28 @@ export default function Dashboard() {
 
     useEffect(() => {
         api.get('/me')
-        .then(response => console.log(response))
-        .catch(err => console.log(err))
+            .then(response => console.log(response))
+            .catch(err => console.log(err))
     }, [])
 
 
     return (
-        <h1>Dashboard: {user?.email}</h1>
+        <>
+            <h1>Dashboard: {user?.email}</h1>
+
+            <Can roles={['editor']}>
+                <div>Métricas</div>
+
+            </Can>
+
+        </>
     )
 }
 
-export const getServerSideProps = withSSRAuth(async (ctx)=> {
+export const getServerSideProps = withSSRAuth(async (ctx) => {
     const apiClient = setupAPIClient(ctx)
     const response = await apiClient.get('/me')
-    
+
     console.log(response.data)
 
     return {
